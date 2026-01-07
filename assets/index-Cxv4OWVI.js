ad6c84896e71e3d71045bcf24d2a5e74b1fd243a
magicwebkraken <terryvenusdev520@gmail.com>
2026-01-08 04:16:46 +0900

    fsdfsd

--- assets/index-Cxv4OWVI.js
+++ assets/index-Cxv4OWVI.js
@@ -92161,7 +92161,7 @@ function pde() {
     d = [
       {
         name: "NASDAQ666",
-        address: "0xcomingsoon",
+        address: "0x3b458ae6718aae9e63aacc8459dfaea3584cce87",
         symbol: "NDQ",
         decimals: 18,
         chainId: 56,
@@ -92194,7 +92194,7 @@ function pde() {
           theme: f,
           tokenList: d,
           provider: i,
-          defaultTokenOut: "0xcomingsoon",
+          defaultTokenOut: "0x3b458ae6718aae9e63aacc8459dfaea3584cce87",
           defaultTokenIn: "0xe9e7cea3dedca5984780bafc599bd69add087d56",
           feeSetting: p,
           enableRoute: !0,
@@ -97200,7 +97200,7 @@ const vc = {
       ],
     });
   },
-  ly = "0xcomingsoon",
+  ly = "0x3b458ae6718aae9e63aacc8459dfaea3584cce87",
   u1e = () =>
     W.jsxs("section", {
       className: "relative bg-[#1b1b1b] w-full !z-20 py-20",
@@ -97351,7 +97351,7 @@ const vc = {
           className: "font-primary text-center text-white flex justify-center",
           children: [
             W.jsxs("iframe", {
-              src: "https://pancakeswap.finance/swap?outputCurrency=0xcomingsoon&inputCurrency=BNB",
+              src: "https://pancakeswap.finance/swap?outputCurrency=0x3b458ae6718aae9e63aacc8459dfaea3584cce87&inputCurrency=BNB",
               style: {width: "400px", height: "500px"}
             }),
           ],
@@ -97489,13 +97489,13 @@ const vc = {
   NL = [
     {
       img: "/imgs/socials/whitedextools.svg",
-      url: "https://www.dextools.io/app/en/bnb/pair-explorer/0xcomingsoon",
+      url: "https://www.dextools.io/app/en/bnb/pair-explorer/0x3b458ae6718aae9e63aacc8459dfaea3584cce87",
       variant: "w-16 h-16",
       title: "Dextools",
     }, 
     {
       img: "/imgs/socials/whitedexscreener.svg",
-      url: "https://dexscreener.com/bsc/0xcomingsoon",
+      url: "https://dexscreener.com/bsc/0x3b458ae6718aae9e63aacc8459dfaea3584cce87",
       variant: "w-16 h-16",
       title: "Dexscreener",
     },
